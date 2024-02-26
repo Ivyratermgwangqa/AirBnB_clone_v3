d8023401b526293694bb9abcefe531944e615a5b
Lerato Mgwangqa <122816970+Ivyratermgwangqa@users.noreply.github.com>
2024-02-26 20:12:29 +0200

    Update index.py

---
+++ api/v1/views/index.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+""" API endpoints for status and statistics"""
+from flask import jsonify
+from models import storage
+from api.v1.views import app_views
+
+@app_views.route('/status')
+def status():
+    """Status of the API"""
+    return jsonify({"status": "OK"})
+
+@app_views.route('/stats')
+def stats():
+    """Statistics about the data"""
+    return jsonify({
+        "amenities": storage.count("Amenity"),
+        "cities": storage.count("City"),
+        "places": storage.count("Place"),
+        "reviews": storage.count("Review"),
+        "states": storage.count("State"),
+        "users": storage.count("User")
+    })
