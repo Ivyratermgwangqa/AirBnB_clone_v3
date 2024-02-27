525ffe344f4ca875ab47f7f7664308ca87333c11
Lerato Mgwangqa <122816970+Ivyratermgwangqa@users.noreply.github.com>
2024-02-27 04:00:29 +0200

    Update __init__.py

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,20 +1,22 @@
-#!/usr/bin/python3
-"""module views"""
-from flask import Blueprint
-from api.v1.views.index import index
-from api.v1.views.states import states
-from api.v1.views.cities import cities
-from api.v1.views.places import places
-from api.v1.views.places_reviews import reviews
-from api.v1.views.places_amenities import amenities
-from api.v1.views.users import users
-from api.v1.views.amenities import amenities
+# api/v1/views/index.py
 
-app_views = Blueprint("app_views", __name__, url_prefix='/api/v1')
-app_views.register_blueprint(index)
-app_views.register_blueprint(states)
-app_views.register_blueprint(cities)
-app_views.register_blueprint(places)
-app_views.register_blueprint(reviews)
-app_views.register_blueprint(amenities)
-app_views.register_blueprint(users)
+from flask import jsonify
+from api.v1.views import app_views  # Remove this line
+
+@app_views.route('/status')
+def status():
+    """Status of the API"""
+    return jsonify({"status": "OK"})
+
+
+@app_views.route('/stats')
+def stats():
+    """Statistics about the data"""
+    return jsonify({
+        "amenities": storage.count("Amenity"),
+        "cities": storage.count("City"),
+        "places": storage.count("Place"),
+        "reviews": storage.count("Review"),
+        "states": storage.count("State"),
+        "users": storage.count("User")
+    })
