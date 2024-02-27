b6b18e5776af09c8d66f15af66580e4fd03db83e
Lerato Mgwangqa <122816970+Ivyratermgwangqa@users.noreply.github.com>
2024-02-27 22:32:00 +0200

    Update app.py

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,27 +1,34 @@
 #!/usr/bin/python3
-"""app.py to connect to API"""
+""" app.py file """
 import os
 from models import storage
+from flask import Flask, jsonify
 from api.v1.views import app_views
-from flask import Flask, Blueprint, jsonify, make_response
 from flask_cors import CORS
-
+"""from flasgger import Swagger"""
 
 app = Flask(__name__)
+"""swagger = swagger(app)"""
+db = os.environ.get('HBNB_TYPE_STORAGE', 'json_file')
+host = os.environ.get('HBNB_API_HOST', '0.0.0.0')
+port = os.environ.get('HBNB_API_PORT', '5000')
+app.url_map.strict_slashes = False
 app.register_blueprint(app_views)
-cors = CORS(app, resources={"/*": {"origins": "0.0.0.0"}})
+CORS(app, resources={r"/api/v1/*": {"origins": "0.0.0.0"}})
 
 
 @app.teardown_appcontext
-def teardown_appcontext(code):
-    """teardown_appcontext"""
+def close(cls):
+    """close"""
     storage.close()
 
 
 @app.errorhandler(404)
-def page_not_found(error):
-    return make_response(jsonify({'error': 'Not found'}), 404)
+def not_found(e):
+    """error 404"""
+    return jsonify({"error": "Not found"}), 404
 
-if __name__ == "__main__":
-    app.run(host=os.getenv('HBNB_API_HOST', '0.0.0.0'),
-            port=int(os.getenv('HBNB_API_PORT', '5000')))
+
+if __name__ == '__main__':
+    """Initialize api"""
+    app.run(host=host, port=int(port), threaded=True)
